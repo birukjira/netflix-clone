e954ace1781a7f2e8dcbd133b88c80fd10e77f95
birukjira <birukjira@gmail.com>
2024-09-12 20:36:34 +0300

    reset the mode and the installation

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,6 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { base: '/netflix-clone' },
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
